9121e795594e3e89cd3720b20565af5b5343839d
Armson0911 <tumwekwatsearmson@gmail.com>
2024-09-21 13:29:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name=" TUMWEKWATSE ARMSON";
+$DOB="09/11/1999";
+$Address=" RWAMPARA";
+$CYear=2024;
+$BYear=1999;
+$Age=$CYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address:".$Address."<br>";
+echo"Welcome home: ".$name."<br>";
+echo"You are now , ".$Age." Years old";
+/* DOB represents date of birth,
+CYEAR means current year
+BYEAR means year of birth*/
+?> 
+
